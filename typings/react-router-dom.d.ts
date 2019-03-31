c78e8c01e06e2fc83cec17029356dce1e58b0dd2
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-04-01 04:24:34 +0800

    remove unrelated file

--- typings/react-router-dom.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export interface BrowserRouterProps {
-  basename?: string;
-  getUserConfirmation?: (message: string, callback: (ok: boolean) => void) => void;
-  forceRefresh?: boolean;
-  keyLength?: number;
-}
-export class BrowserRouter extends React.Component<BrowserRouterProps, any> {}
